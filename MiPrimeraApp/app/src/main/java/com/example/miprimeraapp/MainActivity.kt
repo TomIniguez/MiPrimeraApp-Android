d9729e93d09934a1e377c9028144f14ef3e1b0ba
TomIniguez <lightningstom@yahoo.com>
2026-09-04 19:25:37 -0300

    Ejercicio 5 Terminado

--- MiPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ MiPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.miprimeraapp.ui.theme.MiPrimeraAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,17 +25,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MiPrimeraAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        DatoEstudiante("Nombre", "Ana")
-                        DatoEstudiante("Carrera", "Sistemas")
-                        DatoEstudiante("Anio", "1")
-                        Text(descripcionEdad(30))
-                        Text(descripcionEdadModif(15))
-                        Text(descripcionEdadModif(24))
-
-                    }
+                Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+                    Text(
+                        text = "Perfil",
+                        style = MaterialTheme.typography.titleLarge,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(top = 32.dp, bottom = 8.dp)
+                    )
+                    DatoEstudiante("Nombre", "Ana")
+                    DatoEstudiante("Carrera", "Sistemas")
+                    DatoEstudiante("Anio", "1")
+                    Text(descripcionEdad(30))
+                    Text(descripcionEdadModif(15))
+                    Text(descripcionEdadModif(24))
                 }
+
             }
         }
     }
@@ -38,7 +48,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DatoEstudiante(etiqueta: String, valor: String) {
-    Text("$etiqueta: $valor")
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        horizontalArrangement = Arrangement.SpaceBetween // Separa etiqueta a la izquierda y valor a la derecha
+    ){
+        Text("$etiqueta: $valor")
+    }
 }
 
 
@@ -54,6 +71,7 @@ fun descripcionEdad(edad: Int): String {
     return "Edad: $edad anios"
 }
 
+//Calculador de edad
 fun descripcionEdadModif(edad: Int): String {
     if (edad < 18){
         return "Mi edad es $edad anios y soy menor de edad"
